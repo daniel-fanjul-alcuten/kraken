066ee23a31c795424c389b645d9d76e25c00f45d
Daniel Fanjul <daniel.fanjul.alcuten@gmail.com>
2013-08-04 13:53:58 +0200

    Add Graph

---
+++ kraken-graph/graph.go
@@ -0,0 +1,50 @@
+package main
+
+//// to compile a go get-able repository
+type gojob struct {
+	importPath string
+	request    *request
+}
+
+// a request, refs/requests/*
+type request struct {
+	ref  string
+	time int64
+	// jobs in undefined order
+	jobs      []gojob
+	repoquest *repoquest
+	reference *reference
+}
+
+// a repository with requests
+type repoquest struct {
+	name     string
+	requests map[string]*request
+	graph    *graph
+}
+
+// the ref in the original repository
+type reference struct {
+	name       string
+	srequests  []map[*request]*request
+	mrequests  map[int64]map[*request]*request
+	repository *repository
+}
+
+// a repository with references
+type repository struct {
+	name       string
+	references map[string]*reference
+	graph      *graph
+}
+
+type graph struct {
+	repoquests   map[string]*repoquest
+	repositories map[string]*repository
+}
+
+func newGraph() *graph {
+	repoquests := make(map[string]*repoquest)
+	repositories := make(map[string]*repository)
+	return &graph{repoquests, repositories}
+}
---
+++ kraken-graph/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"flag"
+	"log"
+)
+
+func main() {
+
+	log.SetFlags(0)
+
+	flag.Parse()
+}
